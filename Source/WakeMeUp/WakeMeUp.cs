3f11c4f5a612d94d9ed3b88fcdfd0b7c05d14ad7
Petoovee <Petomatick@gmail.com>
2022-05-23 21:46:11 +0200

    I dont even know

--- Source/WakeMeUp/WakeMeUp.cs
+++ Source/WakeMeUp/WakeMeUp.cs
@@ -9,8 +9,28 @@ namespace WakeMeUp{
 
     class WakeMeUp : ThingComp
     {
-        public override void TickRare() {
+        public override void CompTick() {
             Log.Error("Let's error on every tick!");
+        }
+        public WakeMeUpProperties Props => (WakeMeUpProperties)this.props;
+        public bool ExampleBool => Props.myExampleBool;
+    }
+    public class WakeMeUpProperties : CompProperties
+    {
+        public bool myExampleBool;
+        public float myExampleFloat;
+
+        /// <summary>
+        /// These constructors aren't strictly required if the compClass is set in the XML.
+        /// </summary>
+        public WakeMeUpProperties()
+        {
+            this.compClass = typeof(WakeMeUp);
+        }
+
+        public WakeMeUpProperties(Type compClass) : base(compClass)
+        {
+            this.compClass = compClass;
         }
     }
 }
